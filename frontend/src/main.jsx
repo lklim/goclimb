deab45d1254f291152e9be5f58ab1a12c0b1ccb6
lklim <lklim1@yahoo.com>
2025-04-22 02:59:33 +0800

    Update main.jsx
    /* /index.html

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,15 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
- 
+  <StrictMode>
     <App />
-
+  </StrictMode>,
 )
 
 /*
- <StrictMode>
+https://render.com/docs/deploy-create-react-app?_gl=1*6ialf8*_gcl_au*NTY1MDE5MTc1LjE3NDUyNTgzODE.*_ga*MTY4MDA0MzM5LjE3NDUyNTgzODE.*_ga_QK9L9QJC5N*MTc0NTI1ODM4MC4xLjEuMTc0NTI2MTI4Ni4zNy4wLjA.#using-client-side-routing
 
-</StrictMode>,
+
+
 
    */
