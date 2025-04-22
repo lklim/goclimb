40ea8bc5025998645446a5fc14a04e045ac76b99
lklim <lklim1@yahoo.com>
2025-04-22 17:08:07 +0800

    Update api.js
    http://hsch2ch20h.pythonanywhere.com/api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "/api"; // Proxy to Django
+const API_BASE_URL = "http://hsch2ch20h.pythonanywhere.com/api"; // Proxy to Django
 
 export const fetchData = async () => {
   try {
